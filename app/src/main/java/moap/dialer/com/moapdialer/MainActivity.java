9cb9f36514c558892afa92f976c30020129cf45f
MasoudGit <masoud@localhost.com>
2016-04-28 22:21:53 +0430

    init1

---
+++ app/src/main/java/moap/dialer/com/moapdialer/MainActivity.java
@@ -0,0 +1,13 @@
+package moap.dialer.com.moapdialer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
